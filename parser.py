59f61af04e0c538fc4406f27d1cbe19b875a934d
Bond <bondarenko.jenek@mail.ru>
2021-11-05 17:49:29 +0300

    Get dynamic page layout
    Generate a dynamic markup of the site to get the information of all the necessary posts, as well as hidden tags. Scroll the page to render the posts. Write the username to the result file

--- parser.py
+++ parser.py
@@ -1,10 +1,10 @@
-import requests
 from bs4 import BeautifulSoup
 import logging
 import datetime
 import uuid
-import json
-import pprint
+from selenium import webdriver
+import time
+
 """A module containing a class for parsing a page and logging"""
 format = '%(asctime)s %(lineno)s %(levelname)s:%(message)s'
 logging.basicConfig(format=format, level=logging.DEBUG)
@@ -14,7 +14,7 @@ logger = logging.getLogger('reddit')
 class Client:
     """Create methods for parsing
 
-    The class contains methods that allow you to load a page,
+    The class contains methods that allow you to get a dynamic page,
     parse it and roadblocks, save the result to a file
     and a method to run the parser.
     """
@@ -23,38 +23,53 @@ class Client:
         """Сlass constructor
 
         Arguments:
-        session - an object for saving certain parameters in requests to the same site
-        session.headers - overridden request header in order not to be suspected of parsing
+        url - path to site
         result - list of saved parsing parameters.
         """
-        self.session = requests.Session()
-        self.session.headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                          'Chrome/95.0.4638.69 Safari/537.36',
-            'Accept-Language': 'ru',
-        }
+        self.url = url = 'https://www.reddit.com/top?t=month'
         self.result: list[str] = []
 
-    def load_page(self) -> str:
-        """Return page markup
+    def get_source_html(self):
+        """Generate dynamic markup of the site
 
-        Sends a request to the site by url and gets the markup,
+        Sends a request to the site by url, scrolls down the page
+        and gets the dynamic markup,
+        which is written to the file,
         otherwise generates an exception.
-         Returns the page layout.
         """
-        url = 'https://www.reddit.com/top?t=month'
-        response = self.session.get(url=url)
-        response.raise_for_status()
-        return response.text
+        options = webdriver.ChromeOptions()
+        options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, "
+                             "like Gecko) Chrome/95.0.4638.69 Safari/537.36")
+        driver = webdriver.Chrome(executable_path='resourсes/chromedriver.exe', options=options)
+        driver.maximize_window()
+        try:
+            driver.get(url=self.url)
+            time.sleep(3)
+            while True:
+                find_posts = driver.find_elements_by_class_name('_1oQyIsiPHYt6nx7VOmd1sz')
+                if len(find_posts) >= 100:
+                    with open('resourсes/source-page.html', 'w', encoding="utf-8") as file:
+                        file.write(driver.page_source)
+                    break
+                else:
+                    driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+                    time.sleep(3)
+        except Exception as _ex:
+            print(_ex)
+        finally:
+            driver.close()
+            driver.quit()
 
-    def parse_page(self, text: str) -> None:
+    def parse_page(self, path: str) -> None:
         """Parsing a page block
 
         Gets blocks of posts from the site
         and calls the function parse_block() for each block.
-        Accepts page layout.
+        Takes the path to page markup.
         """
-        soup = BeautifulSoup(text, 'lxml')
+        with open(path, encoding='utf-8') as file:
+            src = file.read()
+        soup = BeautifulSoup(src, 'lxml')
         container = soup.select(
             'div._1oQyIsiPHYt6nx7VOmd1sz', limit=100)
         for block in container:
@@ -65,17 +80,24 @@ class Client:
 
         Gets the necessary information from the post
         and saves it to the result list.
-        Logs errors in case of missing information
+        Logs errors in case of missing information.
         """
-
-        url = 'https://reddit.com' + block.select_one('a.SQnoC3ObvgnGjWt90zD9Z').get('href')
-
+        block_url = block.select_one('a.SQnoC3ObvgnGjWt90zD9Z')
+        if not block_url:
+            logger.error('no block url')
+            return
+        url = 'https://reddit.com' + block_url.get('href')
         if not url:
             logger.error('no elements')
             return
+        username = block.select_one('a._2tbHP6ZydRpjI44J3syuqC').text[2:]
+        if username == '[deleted]':
+            logger.error('no username')
+            return
+        logger.info(block_url)
         logger.info(url)
-
-        self.result.append(f"{self.get_unique_id()},{url}\n")
+        logger.info(username)
+        self.result.append(f'{self.get_unique_id()};{url};{username}\n')
 
     def get_unique_id(self):
         """Generate and return a unique id"""
@@ -92,11 +114,11 @@ class Client:
 
     def run(self) -> None:
         """ Run the parser"""
-        text = self.load_page()
-        self.parse_page(text=text)
+        self.get_source_html()
+        self.parse_page(path='resourсes/source-page.html')
         self.save_result()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = Client()
     parser.run()
