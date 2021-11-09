db5b173d1c3abcf3656b745e8e414b8d9252445a
Bond <bondarenko.jenek@mail.ru>
2021-11-09 22:25:19 +0300

    Add a method to getting user information
    Create a method to start selenium so as not to be repetitive. Also create a method to go to each user and get the necessary information. Add a method to handle the age verification restriction.

--- parser.py
+++ parser.py
@@ -4,8 +4,10 @@ import time
 import uuid
 from bs4 import BeautifulSoup, ResultSet
 from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.action_chains import ActionChains
 
-"""A module containing a class for parsing a page and logging"""
+"""A module containing a class for parsing a site and logging"""
 format = '%(asctime)s %(lineno)s %(levelname)s:%(message)s'
 logging.basicConfig(format=format, level=logging.DEBUG)
 logger = logging.getLogger('reddit')
@@ -15,38 +17,40 @@ class Client:
     """Create methods for parsing
 
     The class contains methods that allow you to get a dynamic page,
-    searching article blocks and parse it, save the result to a file
+    searching article blocks and parse it, пo to a user page and parse it,
+    save the result to a file
     and a method to run the parser.
     """
 
-    def __init__(self) -> None:
+    def __init__(self):
         """Сlass constructor
 
         Arguments:
         url - path to site,
         result - list of saved parsing parameters,
-        user_urls - list of urls to user accounts.
         """
         self.url = url = 'https://www.reddit.com/top?t=month'
-        self.user_urls = []
         self.result: list[str] = []
 
-    def get_result(self):
+    def start_selenium(self) -> WebDriver:
+        """Create a Chrome Web Driver """
+        options = webdriver.ChromeOptions()
+        options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, "
+                             "like Gecko) Chrome/95.0.4638.69 Safari/537.36")
+        driver = webdriver.Chrome(executable_path='resourсes/chromedriver.exe', options=options)
+        return driver
+
+    def get_result(self) -> None:
         """Searching for posts and getting results
 
         Sends a request to the site by url, scrolls down the page
         and gets the dynamic markup.
         Searching article blocks and calling the parsing method of each block.
         """
-        options = webdriver.ChromeOptions()
-        options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, "
-                             "like Gecko) Chrome/95.0.4638.69 Safari/537.36")
-        options.add_argument('--headless')
-        driver = webdriver.Chrome(executable_path='resourсes/chromedriver.exe', options=options)
-        driver.maximize_window()
         try:
+            driver = self.start_selenium()
+            driver.maximize_window()
             driver.get(url=self.url)
-            time.sleep(3)
             initial_post_count = 0
             while True:
                 container = self.parse_page(driver.page_source)
@@ -60,7 +64,6 @@ class Client:
                         self.parse_block(container[i])
                 initial_post_count = last_post_count
                 driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-                time.sleep(3)
         except Exception as _ex:
             print(_ex)
         finally:
@@ -71,8 +74,7 @@ class Client:
         """Parsing a page block
 
         Gets blocks of posts from the site
-        and calls the function parse_block() for each block.
-        Takes the path to page markup.
+        and and his return.
         """
         soup = BeautifulSoup(text, 'lxml')
         container = soup.select(
@@ -83,28 +85,71 @@ class Client:
         """Parsing a single post
 
         Gets the necessary information from the post
-        and saves it to the result list.
+        and the user account, saves it to the result list.
         Logs errors in case of missing information.
         """
         uniq_id = self.get_unique_id()
-        url = self.get_url(block)
+        url = self.get_post_url(block)
         user_name = self.get_user_name(block)
         user_url = self.get_user_url(block)
-        post_date = None
+        post_date = self.get_post_date(block)
         count_comments = self.get_count_comments(block)
         count_of_votes = self.get_count_of_votes(block)
         post_category = self.get_post_category(block)
-        if None in [url, user_name, count_comments, count_of_votes, post_category]:
+        if None in [url, user_name, count_comments, count_of_votes, post_date, post_category]:
             return
-        self.user_urls.append(user_url)
-        self.result.append(f'{uniq_id};{url};{user_name};{post_date};'
+        user_data = self.parse_user_account(user_url)
+        if user_data is None:
+            return
+        self.result.append(f'{uniq_id};{url};{user_name};{user_data["karma"]};{user_data["cake day"]};'
+                           f'{user_data["post karma"]};{user_data["comment karma"]};{post_date};'
                            f'{count_comments};{count_of_votes};{post_category}\n')
+
+    def parse_user_account(self, user_url) -> dict:
+        """Parsing a user account
+
+            Getting the necessary information about the user (also in hidden blocks).
+            Return the dictionary with the user's data.
+            Logs errors in case of missing information.
+            """
+        driver = self.start_selenium()
+        driver.maximize_window()
+        driver.get(url=user_url)
+        try:
+            block_to_hover = driver.find_element_by_id("profile--id-card--highlight-tooltip--karma")
+        except Exception as _ex:
+            print(_ex)
+            return
+        hover = ActionChains(driver).move_to_element(block_to_hover)
+        hover.perform()
+        soup = BeautifulSoup(driver.page_source, 'lxml')
+        block = soup.select_one('div._3odBTM7RqvRgN1nvkf5k8B')
+        block_karma = soup.select_one('div._3uK2I0hi3JFTKnMUFHD2Pd')
+        try:
+            if block and block_karma:
+                karma = self.get_karma(block)
+                cake_day = self.get_cake_day(block)
+                post_and_comment_karma = self.get_post_and_comment_karma(block_karma)
+
+                if None in [karma, cake_day, post_and_comment_karma]:
+                    return
+                return dict([('karma', karma), ('cake day', cake_day), ('post karma', post_and_comment_karma[0]),
+                             ('comment karma', post_and_comment_karma[1])])
+            else:
+                logger.error('age limit')
+                return
+        except Exception as _ex:
+            print(_ex)
+        finally:
+            driver.close()
+            driver.quit()
 
     def get_unique_id(self):
         """Generate and return a unique id"""
         return str(uuid.uuid1().hex)
 
-    def get_url(self, block):
+    def get_post_url(self, block) -> str or None:
+        """Get and return post url"""
         block_url = block.select_one('a.SQnoC3ObvgnGjWt90zD9Z')
         if not block_url:
             logger.error('no block url')
@@ -113,51 +158,79 @@ class Client:
         if not url:
             logger.error('no elements')
             return
-        logger.info(url)
         return url
 
-    def get_user_name(self, block):
+    def get_user_name(self, block) -> str or None:
+        """Get and return user name"""
         user_name = block.select_one('a._2tbHP6ZydRpjI44J3syuqC')
         if user_name == '[deleted]' or not user_name:
             logger.error('no user name')
             return
-        logger.info(user_name)
+        logger.info(user_name.text)
         return user_name.text[2:]
 
-    def get_user_url(self, block):
+    def get_user_url(self, block) -> str or None:
+        """Get and return user url"""
         user_url = block.select_one('a._2tbHP6ZydRpjI44J3syuqC')
         if not user_url:
             logger.error('no user url')
             return
-        logger.info(user_url)
         return 'https://reddit.com' + user_url.get('href')
 
-    def get_post_date(self, block):
-        pass
+    def get_post_date(self, block) -> str or None:
+        """Get and return post date"""
+        day_ago = block.select_one('a._3jOxDPIQ0KaOWpzvSQo-1s')
+        if not day_ago:
+            logger.error('no post date')
+            return
+        return datetime.date.today() - datetime.timedelta(days=int(day_ago.text.split()[0]))
 
-    def get_count_comments(self, block):
-        count_comments = block.find('span', class_='FHCV02u6Cp2zYL0fhQPsO').text
+    def get_count_comments(self, block) -> str or None:
+        """Get and return count comments"""
+        count_comments = block.find('span', class_='FHCV02u6Cp2zYL0fhQPsO')
         if not count_comments:
             logger.error('no comments')
             return
-        logger.info(count_comments)
-        return count_comments
+        return count_comments.text
 
-    def get_count_of_votes(self, block):
-        count_of_votes = block.find('div', class_='_1rZYMD_4xY3gRcSS3p8ODO').text
+    def get_count_of_votes(self, block) -> str or None:
+        """Get and return count of votes"""
+        count_of_votes = block.find('div', class_='_1rZYMD_4xY3gRcSS3p8ODO')
         if not count_of_votes:
             logger.error('no votes')
             return
-        logger.info(count_of_votes)
-        return count_of_votes
+        return count_of_votes.text
 
-    def get_post_category(self, block):
+    def get_post_category(self, block) -> str or None:
+        """Get and return post category"""
         post_category = block.select_one('a._3ryJoIoycVkA88fy40qNJc')
         if not post_category:
             logger.error('no category')
             return
-        logger.info(post_category)
         return post_category.get('href')[3:-1]
+
+    def get_karma(self, block) -> str or None:
+        """Get and return user karma"""
+        karma = block.select_one('span._1hNyZSklmcC7R_IfCUcXmZ')
+        if not karma:
+            logger.error('no karma')
+            return
+        return karma.text
+
+    def get_cake_day(self, block) -> str or None:
+        """Get and return user cake day"""
+        cake_day = block.select_one('span#profile--id-card--highlight-tooltip--cakeday')
+        if not cake_day:
+            logger.error('no cake_day')
+            return
+        return cake_day.text
+
+    def get_post_and_comment_karma(self, block_karma) -> str or None:
+        """Get and return user post karma and user comment karma"""
+        if not block_karma:
+            logger.error('no post and comment carma')
+            return
+        return [block_karma.text.split()[0], block_karma.text.split()[3]]
 
     def save_result(self) -> None:
         """Save the result to a file
