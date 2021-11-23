2e3d4d5066b5c97b4444a35e56d0bf8ad5ac2129
Bond <bondarenko.jenek@mail.ru>
2021-11-23 17:12:46 +0300

    Add a POST method handler
    Create handler for POST method to add string to file, if there is no such string with id yet. Add annotations to the code. Break the code into blocks for readability

--- server.py
+++ server.py
@@ -1,61 +1,117 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import logging
-import json
 import datetime
+import json
+import logging
 import re
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from typing import Dict, List, Tuple, Optional, NoReturn, Pattern
+
+PORT: int = 8087
+NAME_FILE: str = f"reddit-{datetime.datetime.today().strftime('%Y%m%d')}.txt"
+DATA: Tuple[str, ...] = ('unique id', 'post URL', 'username', 'user karma', 'user cake day', 'post karma',
+                         'comment karma', 'post date', 'number of comments', 'number of votes', 'post category')
+UUID4HEX: Pattern = re.compile('[0-9a-f]{12}1[0-9a-f]{19}')
 
 format_log: str = '%(asctime)s %(lineno)s %(levelname)s:%(message)s'
 logging.basicConfig(format=format_log, level=logging.DEBUG)
 logger: logging.Logger = logging.getLogger('logger')
 
-PORT = 8087
-NAME_FILE: str = f"reddit-{datetime.datetime.today().strftime('%Y%m%d')}.txt"
-DATA = ('unique id', 'post URL', 'username', 'user karma', 'user cake day', 'post karma', 'comment karma', 'post date',
-        'number of comments', 'number of votes', 'post category')
-
 
 class RequestHandler(BaseHTTPRequestHandler):
 
-    def read_file(self):
+    def read_file(self) -> Optional[List[Dict[str, str]]]:
         try:
             with open(NAME_FILE, 'r') as file:
-                list_posts = [post.split(';') for post in file.readlines()]
-            result = [{DATA[i]: post[i] for i in range(len(post) - 1)} for post in list_posts]
-            return result
+                list_posts: List[List[str]] = [post.split(';') for post in file.readlines()]
+            return [{DATA[i]: post[i] for i in range(len(post) - 1)} for post in list_posts]
         except Exception as _ex:
             logger.error(_ex)
+            return None
 
-    def write_file(self):
-        pass
+    def write_file(self, post: Dict[str, str]) -> Optional[int]:
+        try:
+            with open(NAME_FILE, 'a+') as file:
+                file.write(f"{';'.join(post.values())};\n")
+            return len(open(NAME_FILE, 'r').readlines())
+        except Exception as _ex:
+            logger.error(_ex)
+            return None
 
-    def do_GET(self):
-        uuid4hex = re.compile('[0-9a-f]{12}1[0-9a-f]{19}')
+    def match_check_id(self, new_post: Dict[str, str]) -> bool:
+        list_posts: Optional[List[Dict[str, str]]] = self.read_file()
+        if list_posts is not None:
+            for post in list_posts:
+                if new_post['unique id'] == post['unique id']:
+                    logger.error('post with this unique id already exists')
+                    return False
+            return True
+        return False
+
+    def do_GET(self) -> NoReturn:
+        data: Optional[List[Dict[str, str]]] = self.read_file()
+
+        if data is not None:
+
+            if self.path == '/posts/':
+                self.send_response(200)
+                logger.info('response sent')
+
+            elif re.search(UUID4HEX, self.path) is not None:
+                logger.info('run')
+
+                for post in data:
+                    logger.info(post['unique id'])
+
+                    if self.path[7:-1] == post['unique id']:
+                        self.send_response(200)
+                        data: Dict[str, str] = post
+                        logger.info('response sent')
+                        break
+
+                    else:
+                        self.send_response(404)
+                        data: str = 'Not found'
+                        break
+
+            else:
+                logger.error('unknown path for GET')
+                data = None
+
+        self.send_header('content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(data).encode('utf-8'))
+
+    def do_POST(self) -> NoReturn:
         if self.path == '/posts/':
-            self.send_response(200)
-            data = self.read_file()
-        elif re.search(uuid4hex, self.path) is not None:
-            logger.info('run')
-            data = self.read_file()
-            for post in data:
-                logger.info(post['unique id'])
-                if self.path[7:-1] == post['unique id']:
-                    self.send_response(200)
-                    data = post
-                    break
+            length = int(self.headers.get('content-length'))
+            new_post: Dict[str, str] = json.loads(self.rfile.read(length))
+
+            if self.match_check_id(new_post):
+                row_number: Optional[int] = self.write_file(new_post)
+
+                if row_number is not None:
+                    self.send_response(201)
+                    logger.info('Post added')
+                    data: Optional[Dict[str, int]] = {"unique id": row_number}
+
                 else:
-                    self.send_response(404)
                     data = None
-                    break
+
+            else:
+                data = None
+
         else:
+            logger.error('unknown path for POST')
             data = None
-        self.send_header('Content-type', 'application/json')
+
+        self.send_header('content-type', 'application/json')
         self.end_headers()
         self.wfile.write(json.dumps(data).encode('utf-8'))
 
 
-def run_server():
-    server_address = ('', PORT)
-    httpd = HTTPServer(server_address, RequestHandler)
+def run_server() -> NoReturn:
+    server_address: Tuple[str, int] = ('', PORT)
+    httpd: HTTPServer = HTTPServer(server_address, RequestHandler)
     logger.info(f"Server running on port {PORT}")
     httpd.serve_forever()
 
